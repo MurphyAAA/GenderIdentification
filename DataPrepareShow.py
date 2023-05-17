57a58cd9ce8106644f374f2605d42184d0ca0e16
elena-dodici <s229114@studenti.polito.it>
2023-05-17 16:22:25 +0100

    AddGaussian&Plot

---
+++ DataPrepareShow.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.linalg
+from scipy import stats
+
+def mcol(v):
+    return v.reshape((v.size, 1))  # 变成列向量
+
+
+def mrow(v):
+    return v.reshape((1, v.size))
+
+    # print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+
+
+def load(filename):
+    DList = []
+    labelList = []
+    with open(filename) as f:
+        for line in f:
+            try:
+                attrs = line.split(',')[0:12]
+                attrs = mcol(np.array([float(i) for i in attrs]))
+                label = line.split(',')[-1].strip()
+
+                DList.append(attrs)
+                labelList.append(label)
+            except:
+                pass
+    return np.hstack(DList), np.array(labelList, dtype=np.int32)
+
+
+
+def gaussianize(D):
+    # 一行一个feature
+    ylist=[]
+    for ind in range(D.shape[0]):
+        # rank(D[ind,:]) 第ind个feature的rank的数组
+        res = stats.norm.ppf(rank(D[ind,:]),loc=0,scale=1)
+        ylist.append(res)
+        # print(res.mean(),res.std())
+    # y: 11 * 1839
+    y = np.vstack(ylist)
+    return y
+def rank(x):
+    ranks = stats.rankdata(x,method='min') # 一定要是min才是均匀分布！ 不是均匀分布没办法转回高斯，对于一个feature 共N个samples，计算每个sample这个feature的值有多少比他小的，（将N个样本的feature按从小到大排序）
+    return (ranks+1) / (len(x) + 2)
+
+def corrlationAnalysis(D):
+    data = {}
+    for i in range(D.shape[0]):
+        data[i] = D[i]
+
+    df = pd.DataFrame(data)
+    corr_matrix = df.corr();
+    sns.heatmap(corr_matrix,cmap="YlGnBu")
+    print(corr_matrix)
+    plt.savefig('heatmap_D')
+    plt.show()
+
+def plot_hist(D,L):
+    ## D1 (12, N(num of sample which L == 0))
+    D1 = D[:, L == 0]
+    D0 = D[:, L == 1]
+
+    for ind in range(D.shape[0]):
+        plt.figure()
+        plt.hist(D0[ind, :], bins=30, density=True, alpha=0.4, label='male')
+        plt.hist(D1[ind, :], bins=30, density=True, alpha=0.4, label='female')
+        plt.legend()
+        plt.tight_layout()
+       # plt.savefig('GAU_hist_%d.pdf' % ind)
+    plt.show()
+
+
--- main.py
+++ main.py
@@ -1,36 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg
+from scipy import stats
+from DataPrepareShow import *
 
-def mcol(v):
-    return v.reshape((v.size,1)) # 变成列向量
 
-def mrow(v):
-    return v.reshape((1, v.size))
-
-    # print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-def load(filename):
-    DList = []
-    labelList = []
-    with open(filename) as f:
-        for line in f:
-            try:
-                attrs = line.split(',')[0:12]
-                attrs = mcol(np.array([float(i) for i in attrs]))
-                label = line.split(',')[-1].strip()
-
-                DList.append(attrs)
-                labelList.append(label)
-            except:
-                pass
-    return np.hstack(DList), np.array(labelList, dtype=np.int32)
 
 def split_data(D, L, seed=0):
     nTrain = int(D.shape[1] * 2.0 / 3.0)
-    np.random.seed(seed) # 设置种子
+    np.random.seed(seed)  # 设置种子
 
-    idx = np.random.permutation(D.shape[1]) # 将n个samples的索引顺序打乱
+    idx = np.random.permutation(D.shape[1])  # 将n个samples的索引顺序打乱
     idxTrain = idx[0:nTrain]
     idxVal = idx[nTrain:]
     DTR = D[:, idxTrain]
@@ -38,21 +18,24 @@ def split_data(D, L, seed=0):
     LTR = L[idxTrain]
     LVAL = L[idxVal]
     return (DTR, LTR), (DVAL, LVAL)
-def PCA(D, m):
-    mu = mcol(D.mean(1)) # mean of each feature
+
+
+def PCA(D,L, m):
+    mu = mcol(D.mean(1))  # mean of each feature
     # centralized
     DC = D - mu
-    C = np.dot(DC,DC.T)
-    C = C/L.size # 协方差矩阵
+    C = np.dot(DC, DC.T)
+    C = C / L.size  # 协方差矩阵
     U, s, Vh = np.linalg.svd(C)
     # print(f'U: {U.shape}')
 
-    P = U[:,0:m]
+    P = U[:, 0:m]
     DP = np.dot(P.T, D)
     # plot_scatter(DP, L)
     # print(f'DP: {DP.shape}')
     # print(f'L: {L.shape}')
     return DP
+
 
 def LDA(D, L, m):
     N = D.shape[1]
@@ -60,8 +43,8 @@ def LDA(D, L, m):
     SWc = np.zeros((m, m))
     SB = np.zeros((m, m))
     for i in range(2):
-        Dc = D[:, L==i]
-        nc = Dc.shape[1] # 样本数量
+        Dc = D[:, L == i]
+        nc = Dc.shape[1]  # 样本数量
         muc = mcol(Dc.mean(1))
         DCc = Dc - muc
         C = np.dot(DCc, DCc.T)
@@ -74,27 +57,37 @@ def LDA(D, L, m):
     SB /= N
     s, U = scipy.linalg.eigh(SB, SW)
     # print(s)
-    W = U[:, ::-1][:,0:1] # biggest eigenvector 2分类。can only reduced to 1D
+    W = U[:, ::-1][:, 0:1]  # biggest eigenvector 2分类。can only reduced to 1D
     Dp = np.dot(W.T, D)
     # print(Dp.shape)
     return Dp
 
+
 def logpdf_GAU_ND(x, mu, C):  # 概率密度、likelihood，x是未去中心化的原数据
     M = x.shape[0]
     a = M * np.log(2 * np.pi)
-    _, b = np.linalg.slogdet(C)  #log|C| 矩阵C的绝对值的log  返回值第一个是符号，第二个是log|C|
+    _, b = np.linalg.slogdet(C)  # log|C| 矩阵C的绝对值的log  返回值第一个是符号，第二个是log|C|
     xc = (x - mu)
     print(mu.shape)
     # xc应该每行循环列数次
-    c = np.dot(xc.T, np.linalg.inv(C)) #np.linalg.inv求矩阵的逆
+    c = np.dot(xc.T, np.linalg.inv(C))  # np.linalg.inv求矩阵的逆
 
     c = np.dot(c, xc)
 
     c = np.diagonal(c)  # 点乘完了取对角线就ok
-    return (-1.0 / 2.0) * (a + b + c) # 密度函数的log
+    return (-1.0 / 2.0) * (a + b + c)  # 密度函数的log
+
+def logpdf_GAU_ND_fast(X,mu,C):
+    XC = X- mu
+    M = X.shape[0]
+    const = -0.5 * M * np.log(2*np.pi)
+    logdet = np.linalg.slogdet(C)[1]
+    L = np.linalg.inv(C)
+    v = (XC * np.dot(L,XC)).sum(0)
+    return const - 0.5 * logdet - 0.5 * v
 def MVG(DTR, LTR, DTE):
-    DTR0 = DTR[:,LTR == 0] # 0类的所有Data
-    DTR1 = DTR[:,LTR == 1] # 1类的所有Data
+    DTR0 = DTR[:, LTR == 0]  # 0类的所有Data
+    DTR1 = DTR[:, LTR == 1]  # 1类的所有Data
     mu0 = mcol(DTR0.mean(1))
     mu1 = mcol(DTR1.mean(1))
     # 去中心化
@@ -104,15 +97,16 @@ def MVG(DTR, LTR, DTE):
     C0 = np.dot(DTRc0, DTRc0.T) / DTRc0.shape[1]
     C1 = np.dot(DTRc1, DTRc1.T) / DTRc1.shape[1]
     # likelihood
-    tll0 = np.exp(logpdf_GAU_ND(DTE, mu0, C0))
-    tll1 = np.exp(logpdf_GAU_ND(DTE, mu1, C1))
-    S = np.vstack((tll0, tll1)) # score
-    Priori = 1/3
+    tll0 = np.exp(logpdf_GAU_ND_fast(DTE, mu0, C0))
+    tll1 = np.exp(logpdf_GAU_ND_fast(DTE, mu1, C1))
+    S = np.vstack((tll0, tll1))  # score
+    Priori = 1 / 3
     SJoint = S * Priori
     SMarginal = mrow(SJoint.sum(0))
     SPost = SJoint / SMarginal
     predict = np.argmax(SPost, axis=0)
     return predict
+
 
 def plot_scatter(D, L):
     D0 = D[:, L == 0]
@@ -133,31 +127,60 @@ def plot_scatter(D, L):
     plt.tight_layout()
     plt.show()
 
+
 def computeAccuracy(predictList, L):
     res = []
     for i, pre in enumerate(predictList):
-        if(pre == L[i]):
-            res.append(True) # 预测正确
+        if (pre == L[i]):
+            res.append(True)  # 预测正确
         else:
             res.append(False)
     corr = res.count(True)
     wrong = res.count(False)
+    print(corr)
+    print(wrong)
+    print(len(res))
     acc = corr / len(res)
     err = wrong / len(res)
     return acc, err
 
-if __name__ == '__main__':
-    # Hyperparameters
-    m = 10 # 12D -> 10D 降维后的维度
-    # D [ x0, x1, x2, x3, ...]  xi是列向量，每行都是一个feature
-    D, L = load('./data/Train.txt')
-    D = PCA(D,m) # Dimensionality reduction  12D -> 10D
-    # DTR = LDA(DTR,LTR,m)
 
+
+
+
+def main():
+    D, L = load('./data/Train.txt')
+    ## plot_hist(D,L)
+    # mvg = Models.MVG()
+    ## gaussianize the training data
+    D_after = gaussianize(D)
+    #plot_hist(D_after, L)
+    # corrlationAnalysis(D)
+    D = PCA(D_after, L, 7)  # Dimensionality reduction  12D -> 10D
+    # # DTR = LDA(DTR,LTR,m)
     (DTR, LTR), (DVAL, LVAL) = split_data(D, L)
     DTE, LTE = load('./data/Test.txt')
-
     predict = MVG(DTR, LTR, DVAL)
+    acc, err = computeAccuracy(predict, LVAL)  # acc: 92.5%
+    print("test")
+    print(acc,err)
 
-    acc, err = computeAccuracy(predict, LVAL) # acc: 92.5%
 
+
+
+if __name__ == '__main__':
+
+    main()
+    # Hyperparameters
+    # m = 10  # 12D -> 10D 降维后的维度
+    # # D [ x0, x1, x2, x3, ...]  xi是列向量，每行都是一个feature
+    # D, L = load('./data/Train.txt')
+    # D = PCA(D, m)  # Dimensionality reduction  12D -> 10D
+    # # DTR = LDA(DTR,LTR,m)
+    #
+    # (DTR, LTR), (DVAL, LVAL) = split_data(D, L)
+    # DTE, LTE = load('./data/Test.txt')
+    #
+    # predict = MVG(DTR, LTR, DVAL)
+    #
+    # acc, err = computeAccuracy(predict, LVAL)  # acc: 92.5%
